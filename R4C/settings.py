5232030e18029a915e10025da87a7aa8a4866ecd
Dima <dvdegtyarev@edu.hse.ru>
2023-09-26 00:24:08 +0300

    - apply migrations - update settings

--- R4C/settings.py
+++ R4C/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+
     'customers',
     'orders',
     'robots'
@@ -121,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
