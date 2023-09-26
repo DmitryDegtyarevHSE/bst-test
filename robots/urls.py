6f9893598cc0b6afb04e43937aebfdd1a7392322
Dima <dvdegtyarev@edu.hse.ru>
2023-09-26 04:13:48 +0300

    task 1

---
+++ robots/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('add/', views.RobotView.as_view(), name="robot-view")
+]
--- robots/views.py
+++ robots/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.core.exceptions import ValidationError
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import Robot
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class RobotView(View):
+    http_method_names = ['post']
+
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            robot = Robot(
+                model=data["model"],
+                version=data["version"],
+                created=data["created"],
+            )
+            robot.full_clean()
+            robot.save()
+            return JsonResponse({"message": "created"}, status=200)
+        except (KeyError, json.JSONDecodeError, ValidationError) as e:
+            return JsonResponse({'error': str(e)}, status=400)
